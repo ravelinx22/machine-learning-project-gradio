faa25bfab724c832c46394e5665fb5a4d7a9bc6c
William Ravelo M <drummerwilliam@gmail.com>
2024-05-17 12:27:16 -0500

    new version

--- app.py
+++ app.py
@@ -1,63 +1,78 @@
 import gradio as gr
 from huggingface_hub import InferenceClient
+import gdown
+import os
+import torch
+import re
+from transformers import AutoModel, BertTokenizerFast, TFBertModel, AutoTokenizer, BertTokenizer, AdamW
+import torch.nn as nn
 
-"""
-For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
-"""
-client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
+###############
+# Load model
+###############
+url='https://drive.google.com/file/d/1cT8q8-rU-5T7TEKAB5oGkIAhS6dOD3O9/view?usp=sharing'
+url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+model_file_name = 'best_model.pt'
 
+if not os.path.exists(model_file_name):
+    gdown.download(url, model_file_name, quiet=False)
+else:
+    print('Model already downloaded')
 
-def respond(
-    message,
-    history: list[tuple[str, str]],
-    system_message,
-    max_tokens,
-    temperature,
-    top_p,
-):
-    messages = [{"role": "system", "content": system_message}]
+###############
+# Preprocess helpers
+###############
 
-    for val in history:
-        if val[0]:
-            messages.append({"role": "user", "content": val[0]})
-        if val[1]:
-            messages.append({"role": "assistant", "content": val[1]})
+def preprocess_text(text):
+    # Transform URLs
+    text = re.sub(r'http\S+', '<URL>', text)
 
-    messages.append({"role": "user", "content": message})
+    # Transform other user mentions
+    text = re.sub(r'@\w+', '<USUARIO>', text)
 
-    response = ""
+    # Delete emojis
+    text = re.sub(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251]+', '', text)
+    text = re.sub(r':\)|;\)|:-\)|:-\(|:-\(|:-\*|:-\)|:P|:D|:<|:-P', '<EMOJI>', text)
 
-    for message in client.chat_completion(
-        messages,
-        max_tokens=max_tokens,
-        stream=True,
-        temperature=temperature,
-        top_p=top_p,
-    ):
-        token = message.choices[0].delta.content
+    # Transform hashtags
+    text = re.sub(r'#\w+', '<HASH_TAG>', text)
 
-        response += token
-        yield response
+    return text
 
-"""
-For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
-"""
-demo = gr.ChatInterface(
-    respond,
-    additional_inputs=[
-        gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
-        gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
-        gr.Slider(minimum=0.1, maximum=4.0, value=0.7, step=0.1, label="Temperature"),
-        gr.Slider(
-            minimum=0.1,
-            maximum=1.0,
-            value=0.95,
-            step=0.05,
-            label="Top-p (nucleus sampling)",
-        ),
-    ],
+MAX_LENGHT = 25
+tokenizer = BertTokenizerFast.from_pretrained('dccuchile/bert-base-spanish-wwm-uncased')
+
+def tokenize(data_to_tokenize):
+  return tokenizer.batch_encode_plus(
+    data_to_tokenize,
+    max_length = MAX_LENGHT,
+    pad_to_max_length=True,
+    truncation=True
+  )
+
+###############
+# Prediction
+###############
+
+def prediction(text):
+    text = preprocess_text(text)
+    tokenized_text = tokenize([text])
+
+    result = 0
+    result_to_text = 'Falsa' if result == 0 else 'Verdadera'
+    return f"La noticia es {result_to_text}"
+
+###############
+# Deploy
+###############
+
+iface = gr.Interface(
+    fn=prediction,
+    inputs="text",
+    outputs="label",
+    title="Detector de tweets de noticias falsas",
+    description="Introduce el texto de una noticia publicada en un tweet y descubre si es verdadera o falsa",
 )
 
-
 if __name__ == "__main__":
-    demo.launch()
+    iface.launch()
